26b68126655b1c9a0158e5a27807a9976ca656d3
AluuLL <lulu_aluu@163.com>
2018-09-03 19:12:51 +0800

    filter not in dict

---
+++ tmp/filter_danci.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#-*-coding:UTF-8 -*-
+
+import sys
+import re
+import json
+
+def myfunc(dictfile,infile,outfile):
+	out = open(outfile,'w')
+	with open(dictfile,'r') as reader:
+		dict_list = []
+		for line in reader:
+			line = line.strip()
+			dict_list.append(line)
+	dict_list = json.dumps(dict_list,encoding="utf8",ensure_ascii=False)
+	#print dict_list
+	with open(infile,'r') as input:
+		for line in input:
+			line = line.strip()
+			en_list = re.findall(r"[a-zA-Z]+",line)
+			for word in en_list:
+				if word.lower() not in dict_list:
+					out.write(word.lower()+"\n")
+
+if __name__=='__main__':
+	dictfile = sys.argv[1]
+	inputfile = sys.argv[2]
+	outfile = sys.argv[3]
+	myfunc(dictfile,inputfile,outfile)
